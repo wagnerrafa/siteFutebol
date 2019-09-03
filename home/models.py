65b0952c68a89ee99c004bafc2b566453efdd875
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-09-03 16:33:28 -0300

    bd de dados imagens

--- home/models.py
+++ home/models.py
@@ -4,11 +4,10 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 class Post(models.Model):
-  title = models.CharField(max_length=255)
-  summary = RichTextField()
-  content = RichTextUploadingField()
+  nome = models.CharField(max_length=255)
+  resumo = RichTextField()
   author = models.CharField(max_length=255)
   image = models.ImageField((""), upload_to="")
   created_at = models.DateField(auto_now_add=True)
   def __str__(self):
-    return self.title
+    return self.nome

b1c68c17d64e263b2cafe2c67c9fedc2c0ed8d7c
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-09-03 18:12:15 -0300

    bd de dados imagens

--- home/models.py
+++ home/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
   nome = models.CharField(max_length=255)
   resumo = RichTextField()
+  content = RichTextUploadingField()
   author = models.CharField(max_length=255)
   image = models.ImageField((""), upload_to="")
   created_at = models.DateField(auto_now_add=True)

f3ab7e0cf4129c294d987f65786bc69f0e408efa
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-09-03 17:53:15 -0300

    bd de dados imagens

--- home/views.py
+++ home/views.py
@@ -7,9 +7,3 @@ def home(request):
     noticias = Post.objects.all()
 
     return render(request, 'index.html', {'posts': noticias})
-
-def viewBlog(request, post_id):
-    noticia = Post.objects.get(pk=post_id)
-    lastNews = Post.objects.all()
-
-    return render(request, 'index.html', {'post': noticia, 'blogPosts': lastNews})

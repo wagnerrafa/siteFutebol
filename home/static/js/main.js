90a6525fb52d11892cf2fde1cf61ccdd7da0f791
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-09-11 16:39:44 -0300

    imagem de fundo do historico em slide

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -8,19 +8,49 @@ function abrirMenuMobile() {
 
 btnMenu.onclick = abrirMenuMobile;
 // Fim código acima
-/* 
-let num1 = 0;
-function calcular() {
-casa = document.getElementById("casa");
-console.log(casa);
-fora = document.getElementById("fora");
-console.log(fora);
-if (casa > fora){
-num1=num1++
-}
-let elemResult = document.getElementById("resul");
-    elemResult.textContent = num1++;
-    alert(num1);
-  }
 
-   */
+
+  /*document.getElementById ("mm1").addEventListener ("click", myFunction, false); function myFunction() { document.getElementById("historico").style.backgroundColor = "red"; } */
+
+
+var imageCount = 0;
+var currentImage = 0;
+var images = new Array();
+ 
+images[0] = 'https://res.cloudinary.com/wagner/image/upload/v1568229227/foto1_b2whz1.jpg';
+images[1] = 'https://res.cloudinary.com/wagner/image/upload/v1568229227/foto2_x1ngon.jpg';
+images[2] = 'https://res.cloudinary.com/wagner/image/upload/v1568229227/foto3_gxawxf.jpg';
+
+ 
+var preLoadImages = new Array();
+for (var i = 0; i < images.length; i++)
+{
+   if (images[i] == "")
+      break;
+ 
+   preLoadImages[i] = new Image();
+   preLoadImages[i].src = images[i];
+   imageCount++;
+}
+ 
+function startSlideShow()
+{
+   if (document.body && imageCount > 0)
+   {
+      document.getElementById("historico").style.backgroundImage = "url("+images[currentImage]+")";    
+      document.getElementById("historico").style.backgroundAttachment = "fixed";
+      document.getElementById("historico").style.backgroundRepeat = "no-repeat";
+      document.getElementById("historico").style.backgroundsize = "cover";
+
+ 
+      currentImage = currentImage + 1;
+      if (currentImage > (imageCount-1))
+      { 
+         currentImage = 0;
+      }
+      setTimeout('startSlideShow()', 4000);
+   }
+}
+startSlideShow();
+  
+  
